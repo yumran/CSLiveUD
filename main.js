0f469e7d4d0ed7aaaa81274bfb9853b61fd7229e
yumr <2506772508@qq.com>
2022-03-22 18:07:47 +0800

    '单实例'

--- main.js
+++ main.js
@@ -91,6 +91,21 @@ function createWindow () {
   })
 }
 
+
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // 当运行第二个实例时,将会聚焦到myWindow这个窗口
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+  
+}
+
 // 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
